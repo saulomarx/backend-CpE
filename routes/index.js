975e4025569d4e1cf9768366bd74f81e4d95382c
Saulo Marx <saulo.marx@cobli.co>
2019-04-24 22:09:19 -0300

    add check route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 const { Router } = require('express');
 const pesquisadoresRouter = require('./pesquisadoresRouter');
+const checkRouter = require('./checkRouter');
 
 const router = Router();
 
+// TODO add a way to check the calling routes 
+// router.use((req)=>{
+//   console.log(req.url);
+//   console.log(req.method);
+//
+// })
+
 router.use('/pesquisadores', pesquisadoresRouter);
+router.use('/check', checkRouter);
 
 module.exports = router;
